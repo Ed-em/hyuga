dae7887bb7e9791943597a147f6de65fc2d79fdc
Ed-em <dayonafi@gmail.com>
2021-03-04 19:49:04 +0900

    another try at raty with Amina san

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+25.times do |t|
+    User.create! :name => "Panin Ghana#{t}", :username => "Panin#{t}", :email => "edem#{t}@gmail.com", :password => "mysecret#{t}", :password_confirmation => "mysecret#{t}", :superadmin_role => false, :user_role => true
+end
