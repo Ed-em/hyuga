1014d84d1b3f128fc09117f42dcbbf25aa4ad852
Ed-em <dayonafi@gmail.com>
2021-03-09 14:03:50 +0900

    style

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,6 +11,6 @@ class Movie < ApplicationRecord
   validates_uniqueness_of :title, :case_sensitive => false
 
   paginates_per 4
-  has_attached_file :movie_img, styles: { movie_index: "250x350>", movie_show: "400x600>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :movie_img, styles: { movie_index: "250x350>", movie_image: "400x600>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :movie_img, content_type: /\Aimage\/.*\z/
 end
